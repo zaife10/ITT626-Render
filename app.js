65c0b1fb1ac861d8a92ab13b6ccd0661f28adc75
zaife10 <2020813046@student.uitm.edu.my>
2023-02-08 16:08:32 +0800

    appjs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express =  require('express');
 const path    =  require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-
+const MemoryStore = require('memorystore')(session)
 
 const app = express();
 const port = process.env.PORT || 3000
@@ -16,9 +16,13 @@ const adminRouter = require('./routes/admin');
 
 app.use(
     session({
+      cookie: {maxAge:86400000},
+      store: new MemoryStore({
+        checkPeriod:86400000
+      }),
       secret: 'secret',
-      resave: true,
-      saveUninitialized: false
+      resave: false,
+      saveUninitialized: true
     })
   );
 
